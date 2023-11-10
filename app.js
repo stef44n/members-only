4f0514ac23a13e25fe51b5bc6e4635ee5f6f7c10
Stefan <stefanseifeddine@gmail.com>
2023-11-10 23:00:10 +0000

    Add passport local strategy

--- app.js
+++ app.js
@@ -25,12 +25,81 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const user = await User.findOne({ username: username });
+            if (!user) {
+                return done(null, false, { message: "Incorrect username" });
+            }
+            if (user.password !== password) {
+                return done(null, false, { message: "Incorrect password" });
+            }
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err);
+    }
+});
+
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
+
+app.get("/", (req, res) => {
+    res.render("index", { user: req.user });
+});
+
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+app.get("/log-out", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect("/");
+    });
+});
+
+app.post("/sign-up", async (req, res, next) => {
+    try {
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password,
+        });
+        const result = await user.save();
+        res.redirect("/");
+    } catch (err) {
+        return next(err);
+    }
+});
+
+app.post(
+    "/log-in",
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/",
+    })
+);
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
 
