a5b3efa834da692155c4130acbe40b152a17095d
Stefan <stefanseifeddine@gmail.com>
2023-11-10 17:34:29 +0000

    Set up app for authentication

--- app.js
+++ app.js
@@ -1,52 +1,37 @@
-const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 require("dotenv").config();
+// console.log(process.env);
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const mongoDb = process.env.SECRET_KEY;
+mongoose.connect(mongoDb);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+const User = mongoose.model(
+    "User",
+    new Schema({
+        username: { type: String, required: true },
+        password: { type: String, required: true },
+    })
+);
 
 const app = express();
-
-// Set up mongoose connection
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-const mongoDB = process.env.SECRET_KEY;
-
-main().catch((err) => console.log(err));
-async function main() {
-    await mongoose.connect(mongoDB);
-}
-
-// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.use(logger("dev"));
-app.use(express.json());
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.get("/", (req, res) => res.render("index"));
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
-});
+app.listen(3000, () => console.log("app listening on port 3000!"));
 
 module.exports = app;
