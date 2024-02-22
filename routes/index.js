fdcbad949542560f4082d3060b008723aae3e735
Stefan <stefanseifeddine@gmail.com>
2024-02-22 21:21:00 +0000

    Add user and messages data from mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const Message = require("../models/message");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-    res.render("index", { title: "Express" });
+router.get("/", async (req, res) => {
+    try {
+        // Fetch messages from the database
+        // const messages = await Message.find();
+        const messages = await Message.find().populate("user");
+
+        // Render the view and pass messages data to it
+        res.render("index", { user: req.user, messages: messages });
+    } catch (error) {
+        console.error("Error fetching messages:", error);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 module.exports = router;
